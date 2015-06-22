c22d6a823f3f12facd65dcdd6fe9fa0dba8a793e
Marnel J. Rodriguez <marnel.x.rodriguez@hsbc.ca>
2015-06-22 09:19:21 -0700

    Updated Job Categories View
    - Added camelCase to human angular filter to convert the camel-cased category to human readable string

--- public/app/project/project.js
+++ public/app/project/project.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('project', ['ngRoute', 'ngResource', "oitozero.ngSweetAlert", 'ui.bootstrap']);
+angular.module('project', ['ngRoute', 'ngResource', "oitozero.ngSweetAlert", 'ui.bootstrap', 'camelCaseToHuman']);
