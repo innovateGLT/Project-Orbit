35ee60e1254e78f657d70efacc431a6620d54d21
Marnel J. Rodriguez <whofeih@gmail.com>
2015-06-26 11:59:32 -0700

    Project Structure update and minor bug fixes
    - Updated user module structure
    - some minor fixes regarding category images

---
+++ public/app/user/service/UserService.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// TODO : cleanup
+
+angular.module('project')
+
+    .factory('Users', ['$resource',
+        function($resource) {
+
+            return $resource('/api/users/:id', null, {
+                'update': {
+                    method: 'PUT'
+                },
+                'login': {
+                    method: 'POST',
+                    'url': '/api/users/login',
+                },
+                'get': {
+                    method: 'GET'
+                },
+                'getMatches': {
+                    method: 'GET',
+                    'url': '/api/users/matches',
+                    isArray: true
+                },
+                'featured': {
+                    method: 'GET',
+                    'url': '/api/users/featured',
+                    isArray: true
+                },
+                'upsert': {
+                    method: 'PUT',
+                    'url': '/api/users/upsert/:user_id'
+                },
+                'getByUserId': {
+                    method: 'GET',
+                    'url': '/api/users/by_id/:user_id'
+                },
+                'getByEmail': {
+                    method: 'GET',
+                    'url': '/api/users/by_email/:email'
+                }
+            });
+        }
+    ]);
